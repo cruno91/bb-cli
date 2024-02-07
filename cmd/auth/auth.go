a1a093f67f8e5aac5c15fbcc051538bb329ad607
Chris Runo <chris.runo@gmail.com>
2024-02-06 21:00:17 -0500

    Start setting up Bitbucket authentication with oauth token.

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -2,11 +2,17 @@ package auth
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/ktrysmt/go-bitbucket"
 	"github.com/spf13/cobra"
 )
 
-// CmdAuth represents the diskUsage command
+var (
+	oauthToken string
+)
+
+// CmdAuth represents the auth command
 var CmdAuth = &cobra.Command{
 	Use:   "auth",
 	Short: "Authenticate with a Bitbucket account.",
@@ -16,4 +22,28 @@ var CmdAuth = &cobra.Command{
 	},
 }
 
-func init() {}
+func init() {
+	CmdAuth.Flags().StringVarP(&oauthToken, "token", "t", "", "Bitbucket token (ex: 1234abc:0987654321zyxwvutsrqponmlkjihgfedcba)")
+	if err := CmdAuth.MarkFlagRequired("token"); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func auth(token string) (c *bitbucket.Client) {
+	bitbucketClientId, bitbucketClientSecret, tokenError := splitToken(token)
+
+	if tokenError != nil {
+		fmt.Println(tokenError)
+		return
+	}
+
+	return bitbucket.NewOAuthClientCredentials(bitbucketClientId, bitbucketClientSecret)
+}
+
+func splitToken(input string) (string, string, error) {
+	parts := strings.Split(input, ":")
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("invalid token format")
+	}
+	return parts[0], parts[1], nil
+}
