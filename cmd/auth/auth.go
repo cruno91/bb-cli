237fab2f3a833959916ab8f60705b8e44278a8c2
Chris Runo <chris.runo@gmail.com>
2024-02-07 20:38:58 -0500

    Setup bb auth to read the token from the argument and write it to config.

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -2,10 +2,10 @@ package auth
 
 import (
 	"fmt"
-	"strings"
-
 	"github.com/ktrysmt/go-bitbucket"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"strings"
 )
 
 var (
@@ -18,7 +18,25 @@ var CmdAuth = &cobra.Command{
 	Short: "Authenticate with a Bitbucket account.",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("auth called")
+		if oauthToken != "" {
+			// Save the token in the viper configuration
+			viper.Set("oauth.token", oauthToken)
+
+			// Attempt to save the configuration to file
+			if err := viper.WriteConfig(); err != nil {
+				if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+					// If the config file was not found, try to write it as a new file.
+					if err := viper.SafeWriteConfig(); err != nil {
+						fmt.Printf("Error saving configuration: %s\n", err)
+					}
+				} else {
+					fmt.Printf("Error updating configuration: %s\n", err)
+				}
+			} else {
+				fmt.Println("Authentication token updated successfully.")
+			}
+		}
+		fmt.Println("auth called with token:", oauthToken)
 	},
 }
 
