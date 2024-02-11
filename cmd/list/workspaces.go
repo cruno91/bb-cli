4fbcfd03a2b69e1b742144b623e8ae984659f3af
Chris Runo <chris.runo@gmail.com>
2024-02-10 21:42:21 -0500

    List workspaces given auth.

--- cmd/list/workspaces.go
+++ cmd/list/workspaces.go
@@ -1,7 +1,9 @@
 package list
 
 import (
+	"bb-cli/cmd/auth"
 	"fmt"
+	"github.com/ktrysmt/go-bitbucket"
 	"github.com/spf13/cobra"
 )
 
@@ -12,9 +14,23 @@ var CmdListWorkspaces = &cobra.Command{
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("workspaces called")
+
+		bb := auth.Auth()
+
+		listWorkspaces(bb)
 	},
 }
 
 func init() {
 	CmdList.AddCommand(CmdListWorkspaces)
+}
+
+func listWorkspaces(bb *bitbucket.Client) {
+	workspaces, err := bb.Workspaces.List()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Workspaces:", workspaces)
 }
